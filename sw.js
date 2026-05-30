42e17bceccda74ab409aa1e6866d40605c1ff8ce
tugelanboto19 <imam20220100039@sibermu.ac.id>
2026-05-30 12:32:06 +0700

    jaringan

--- sw.js
+++ sw.js
@@ -10,6 +10,7 @@ const urlsToCache = [
     '/verify.html',
     '/assets/css/style.css',
     '/assets/images/logopma.png',
+    '/network.html'
     // Mengunci pustaka eksternal agar bisa jalan tanpa internet
     'https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/pdf-lib/1.17.1/pdf-lib.min.js',
